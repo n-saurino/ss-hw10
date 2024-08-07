a6fd6ae73e736866ab534419c7d50ef1153cb4d7
Nigel Saurino <nigelsaurino@gmail.com>
2024-08-07 16:04:57 -0500

    Learning about strands in boost for thread synchronization

--- main.cpp
+++ main.cpp
@@ -1,21 +1,33 @@
+#include <functional>
 #include <stdio.h>
+#include <thread>
 #include "ex1/ex1.h"
 
 class Printer
 {
 private:
     int count_;
-    boost::asio::steady_timer timer_;
+    boost::asio::steady_timer timer1_;
+    boost::asio::steady_timer timer2_;
+    boost::asio::strand<boost::asio::io_context::executor_type> strand_;
 
 public:
     Printer(boost::asio::io_context& io);
     ~Printer();
-    void Print();
+    void Print1();
+    void Print2();
 };
 
-Printer::Printer(boost::asio::io_context& io): timer_(io, std::chrono::seconds(1)), count_(0)
+Printer::Printer(boost::asio::io_context& io): strand_(boost::asio::make_strand(io)), 
+                                                timer1_(io, std::chrono::seconds(1)), 
+                                                timer2_(io, std::chrono::seconds(1)), 
+                                                count_(0)
 {
-    timer_.async_wait(std::bind(&Printer::Print, this));
+    timer1_.async_wait(boost::asio::bind_executor(strand_,
+          std::bind(&Printer::Print1, this)));
+    
+    timer2_.async_wait(boost::asio::bind_executor(strand_,
+    std::bind(&Printer::Print2, this)));
 }
 
 Printer::~Printer()
@@ -23,16 +35,25 @@ Printer::~Printer()
     std::cout << "Final count: " << count_ << std::endl;
 }
 
-void Printer::Print(){
-    if((this->count_) < 6){
-        std::cout << count_ << std::endl;
-        this->timer_.expires_at(timer_.expiry() + std::chrono::seconds(1));
-        timer_.async_wait(std::bind(&Printer::Print, this));
+void Printer::Print1(){
+    if((this->count_) < 10){
+        std::cout << "Timer 1: " << count_ << std::endl;
+        this->timer1_.expires_at(timer1_.expiry() + std::chrono::seconds(1));
+        timer1_.async_wait(boost::asio::bind_executor(strand_,
+          std::bind(&Printer::Print1, this)));
         ++count_;
     }
 }
 
-
+void Printer::Print2(){
+    if((this->count_) < 10){
+        std::cout << "Timer 2: " << count_ << std::endl;
+        this->timer2_.expires_at(timer2_.expiry() + std::chrono::seconds(1));
+        timer2_.async_wait(boost::asio::bind_executor(strand_,
+    std::bind(&Printer::Print2, this)));
+        ++count_;
+    }
+}
 
 
 int main(int, char**){
@@ -42,8 +63,12 @@ int main(int, char**){
     // create Printer object
     Printer printer(io);
 
-    // run the io execution context
+    // start a second thread
+    std::thread t([&]{ io.run(); });
+    // run main thread
     io.run();
+    // wait for child thread to complete
+    t.join();
 
     return 0;
 }
