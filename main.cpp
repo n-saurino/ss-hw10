7b5d7c0da263fd9c5e8c366e993a98956fc0c145
Nigel Saurino <nigelsaurino@gmail.com>
2024-08-07 14:09:23 -0500

    Using member functions with asio timer

--- main.cpp
+++ main.cpp
@@ -1,34 +1,49 @@
 #include <stdio.h>
 #include "ex1/ex1.h"
 
-void Print(const boost::system::error_code& /*e*/, boost::asio::steady_timer* timer, int* count){
-    if((*count) < 6){
-        std::cout << *count << std::endl;
-        timer->expires_at(timer->expiry() + std::chrono::seconds(1));
-        timer->async_wait(std::bind(Print,
-          boost::asio::placeholders::error, timer, count));
-        (*count)++;
+class Printer
+{
+private:
+    int count_;
+    boost::asio::steady_timer timer_;
+
+public:
+    Printer(boost::asio::io_context& io);
+    ~Printer();
+    void Print();
+};
+
+Printer::Printer(boost::asio::io_context& io): timer_(io, std::chrono::seconds(1)), count_(0)
+{
+    timer_.async_wait(std::bind(&Printer::Print, this));
+}
+
+Printer::~Printer()
+{
+    std::cout << "Final count: " << count_ << std::endl;
+}
+
+void Printer::Print(){
+    if((this->count_) < 6){
+        std::cout << count_ << std::endl;
+        this->timer_.expires_at(timer_.expiry() + std::chrono::seconds(1));
+        timer_.async_wait(std::bind(&Printer::Print, this));
+        ++count_;
     }
 }
+
+
+
 
 int main(int, char**){
     // create execution context for asynchronous execution
     boost::asio::io_context io;
 
-    // creating int on the stack and passing it by reference to Print function
-    int count = 0;
-
-    // create a timer executor with the io execution context
-    boost::asio::steady_timer timer(io, std::chrono::seconds(1));
-    
-    // set the timer to wait asynchronously and run Print when it finishes
-    timer.async_wait(std::bind(Print,
-          boost::asio::placeholders::error, &timer, &count));
+    // create Printer object
+    Printer printer(io);
 
     // run the io execution context
     io.run();
-
-    std::cout << "Final count: " << count << std::endl;
 
     return 0;
 }
