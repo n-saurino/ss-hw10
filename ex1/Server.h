666195cf7c57e4197ade0fbe12d95b20cb798143
Nigel Saurino <nigelsaurino@gmail.com>
2024-08-08 11:52:48 -0500

    Starting example daytime server/client with boost

---
+++ ex1/Server.h
@@ -0,0 +1,18 @@
+#include <boost/asio.hpp>
+
+class Server
+{
+private:
+    /* data */
+public:
+    Server(/* args */);
+    ~Server();
+};
+
+Server::Server(/* args */)
+{
+}
+
+Server::~Server()
+{
+}
